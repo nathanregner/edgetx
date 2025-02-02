c0ef39ec735d082d92161432a5151e6530e5fd0c
Nathan Regner <nathanregner@gmail.com>
2025-02-02 12:54:11 -0700

    separeate simulator_plugin builds

--- simulator_plugin.nix
+++ simulator_plugin.nix
@@ -1,5 +1,4 @@
 {
-  SDL2,
   clangStdenv,
   dfu-util,
   fetchFromGitHub,
@@ -7,16 +6,17 @@
   libusb1,
   miniz,
   openssl,
-  qtbase,
-  qtmultimedia,
-  qtserialport,
-  qttools,
+  cmake,
+  gnumake,
+  pkg-config,
+  clang-tools,
+  python3,
   yaml-cpp,
   ...
 }:
 
 {
-  plugin_name,
+  target,
 }:
 
 let
@@ -24,7 +24,7 @@ let
 in
 clangStdenv.mkDerivation rec {
   # clangStdenv.mkDerivation rec {
-  pname = "edgetx";
+  pname = "edgetx-libsimulator-${target}";
   version = "2.10.5";
   # src = fetchFromGitHub {
   #   owner = "EdgeTX";
@@ -35,8 +35,7 @@ clangStdenv.mkDerivation rec {
   # };
   src = ./.;
 
-  nativeBuildInputs = with pkgs; [
-    qttools
+  nativeBuildInputs = [
     cmake
     gnumake
     pkg-config
@@ -56,15 +55,11 @@ clangStdenv.mkDerivation rec {
 
   # libsForQt5.callPackage
   buildInputs = [
-    SDL2
-    dfu-util
-    libusb1
-    miniz
-    openssl
-    qtbase
-    qtmultimedia
-    qtserialport
-    yaml-cpp
+    # dfu-util
+    # libusb1
+    # miniz
+    # openssl
+    # yaml-cpp
   ];
 
   dontWrapQtApps = true;
@@ -73,29 +68,37 @@ clangStdenv.mkDerivation rec {
     patchShebangs .
   '';
 
+  env.TARGET = target;
+
   buildPhase = ''
-    cmake -DPCB=X7 -DPCBREV=MT12 .
-    cmake --build . -j"${JOBS}" --target native-configure
-    cmake --build native -j"${JOBS}" --target package
+    COMMON_OPTIONS="-DGVARS=YES -DHELI=YES -DLUA=YES -Wno-dev -DCMAKE_BUILD_TYPE=Release"
+    if [ "$(uname)" = "Darwin" ]; then
+      COMMON_OPTIONS="''${COMMON_OPTIONS} -DCMAKE_OSX_DEPLOYMENT_TARGET='10.15'"
+    fi
+
+    source ../tools/build-common.sh
+
+    # mkdir build
+    # cd build
+
+    BUILD_OPTIONS="''${COMMON_OPTIONS}"
+    if ! get_target_build_options "$TARGET"; then
+        echo "Error: Failed to find a match for target '$target_name'"
+        exit 1
+    fi
+
+    pwd
+    cmake ''${BUILD_OPTIONS} .
+    cmake --build . --target native-configure
+    cmake --build native --target libsimulator
   '';
 
-  cmakeFlags =
-    let
-      maxLibQt = fetchFromGitHub {
-        owner = "mpaperno";
-        repo = "maxLibQt";
-        rev = "b903e7a755b241313b7acdea0258ee17cbd8fc04";
-        hash = "sha256-xKgUIuh6ANsKrih1lK1mKPCzh52RnDJVT4XwQvI97mk=";
-      };
-    in
-    [
-      "-DFETCHCONTENT_SOURCE_DIR_MAXLIBQT=${maxLibQt}"
-      "-DGTEST_ROOT=${gtest.src}/googletest"
-      "-DDFU_UTIL_PATH=${dfu-util}/bin/dfu-util"
-      # file RPATH_CHANGE could not write new RPATH
-      "-DCMAKE_SKIP_BUILD_RPATH=ON"
-      "-DFETCHCONTENT_SOURCE_DIR_MAXLIBQT=${maxLibQt}"
-    ];
+  cmakeFlags = [
+    "-DGTEST_ROOT=${gtest.src}/googletest"
+    "-DDFU_UTIL_PATH=${dfu-util}/bin/dfu-util"
+    # file RPATH_CHANGE could not write new RPATH
+    "-DCMAKE_SKIP_BUILD_RPATH=ON"
+  ];
 
   installPhase = ''
     #
